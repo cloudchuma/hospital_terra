0caeb845e00d9e82f5db2660e1a6fe1ea87ebe73
cloudchuma <chumastummy@gmail.com>
2025-02-12 00:32:50 +0100

    commit of edits to AWS

--- 1_resource_group.tf
+++ 1_resource_group.tf
@@ -1,4 +1,18 @@
-resource "aws_resource_group" "this" {
-  name     = "hipaa-resources-group"
-  location = "West Europe"
+resource "aws_resourcegroups_group" "this" {
+  name = "hipaa-resources-group"
+
+  resource_query {
+    query = jsonencode({
+      ResourceTypeFilters = ["AWS::AllSupported"]
+      TagFilters = [{
+        Key    = "Environment"
+        Values = ["hipaa"]
+      }]
+    })
+  }
+
+  tags = {
+    Name        = "hipaa-resources-group"
+    Environment = "hipaa"
+  }
 }
--- 6_node_group.tf
+++ 6_node_group.tf
@@ -2,7 +2,7 @@ resource "aws_eks_node_group" "this" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = "internal"
   node_role_arn   = aws_iam_role.eks_node.arn
-  subnet_ids      = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]  # Replace with your subnets
+  subnet_ids      = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]  # Ensure these exist
 
   scaling_config {
     desired_size = 2
@@ -11,12 +11,16 @@ resource "aws_eks_node_group" "this" {
   }
 
   instance_types = ["t3.medium"]  # Equivalent of "Standard_DS2_v2"
+  
+  ami_type       = "AL2_x86_64"  # Amazon Linux 2 (Adjust if needed)
+  disk_size      = 20            # Default disk size in GB
+  capacity_type  = "ON_DEMAND"    # Change to "SPOT" for cheaper instances
 
   tags = {
     Environment = "dev"
   }
 
   lifecycle {
-    ignore_changes = [scaling_config[0].desired_size]
+    ignore_changes = [scaling_config]  # Corrected syntax
   }
 }
